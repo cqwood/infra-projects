0b6cd07ceacd467d5d9af2fd1e9719bc6ff53315
Curtis Wood <cqwood223@gmail.com>
2024-04-03 22:43:40 -0400

    fixed static hosting

--- modules/s3-static-htmx/main.tf
+++ modules/s3-static-htmx/main.tf
@@ -1,6 +1,5 @@
 resource "aws_s3_bucket" "example" {
   bucket = "static-htmx-content"
-
   tags = {
     Name        = "My bucket"
     Environment = "Dev"
@@ -17,4 +16,33 @@ resource "aws_s3_bucket_website_configuration" "example" {
   error_document {
     key = "error.html"
   }
+}
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.example.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "example" {
+  bucket = aws_s3_bucket.example.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = "*"
+        Action = [
+          "s3:GetObject"
+        ]
+        Resource = [
+          "${aws_s3_bucket.example.arn}/*"
+        ]
+      }
+    ]
+  })
 }
